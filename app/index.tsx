69ffd5ff352f2acf0ddc3c315483667d5e56c741
Guus <ghligtvoet@gmail.com>
2024-07-11 11:57:39 +0200

    add custom button

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, Text, View } from "react-native";
 import { Link } from "expo-router";
+import CustomButton from '@/components/CustomButton';
 
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
+
   return (
     <View className="flex-1 items-start justify-start bg-white p-10">
       
@@ -20,7 +26,13 @@ export default function Index() {
       <Button title="Add Drink"/>
       <Button title="Add Drink"/>
       <Button title="Add Drink"/>
-      <Button title="Custom..."/>    
+      <Button title="Custom..."/>   
+
+      <CustomButton 
+        title={count}
+        handlePress={onPress}
+        containerStyles="w-full mt-7"
+      />
       
     </View>
   );
