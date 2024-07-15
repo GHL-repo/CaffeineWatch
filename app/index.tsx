7b2e48d7763d3958a0bb0b0e5aef1695db2bd767
Guus <ghligtvoet@gmail.com>
2024-07-15 10:14:28 +0200

    add mg count to btns

--- app/index.tsx
+++ app/index.tsx
@@ -5,35 +5,59 @@ import { Link } from "expo-router";
 import CustomButton from '@/components/CustomButton';
 
 
-export default function Index() {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(prevCount => prevCount + 1);
+
+export default function Index() {  
+  const mgFilter = 95;
+  const mgEspresso = 27;
+  const mgCappuccino = 27;
+  const mgInstant = 82;
+
+
+  const [mgCount, setMgCount] = useState(0);
+  const resetMgCount = () => setMgCount(0);
 
 
   return (
+    
     <View className="flex-1 items-start justify-start bg-white p-10">
       
       <Text className="font-pblack">Index</Text>
       <StatusBar style="auto" />
       <Link className="font-pbold" href="/drinks">Go to my drinks</Link>
       <Link className="font-pbold" href="/profile">Go to profile</Link>
-      <Text className="font-pregular">Current caffeine amount: 420mg</Text>
-      <Text className="font-pregular">void Caffeine after: 16:20</Text>
-
-      <Button title="Add Drink"/>
-      <Button title="Add Drink"/>
-      <Button title="Add Drink"/>
-      <Button title="Add Drink"/>
-      <Button title="Add Drink"/>
-      <Button title="Add Drink"/>
-      <Button title="Custom..."/>   
+      <Text className="font-pregular">Current caffeine amount: {mgCount}mg</Text>
+      <Text className="font-pregular">Avoid Caffeine after: 16:20</Text>
 
       <CustomButton 
-        title={count}
-        handlePress={onPress}
-        containerStyles="w-full mt-7"
+        title="Filter Coffee"
+        handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 95)}
+        containerStyles="w-full mt-3"
       />
-      
+
+      <CustomButton 
+        title="Espresso"
+        handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 27)}
+        containerStyles="w-full mt-3"
+      />  
+
+      <CustomButton 
+        title="Cappuccino"
+        handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 27)}
+        containerStyles="w-full mt-3"
+      />  
+
+      <CustomButton 
+        title="Instant Coffee"
+        handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 82)}
+        containerStyles="w-full mt-3"
+      />  
+    
+      <CustomButton
+      title="Reset"
+      handlePress={resetMgCount}
+      containerStyles="bg-blue-400 w-2/5 mt-3"
+      /> 
+
     </View>
   );
 }
