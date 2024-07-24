68d40f52ea518feacf184462d293e3f51eeec8fa
Guus <ghligtvoet@gmail.com>
2024-07-24 17:47:45 +0200

    added json data

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,46 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, Text, View, Image } from "react-native";
+import { Button, Text, View, Image, Pressable } from "react-native";
 import { Link } from "expo-router";
 import CustomButton from '@/components/CustomButton';
 import user from "../assets/icons/user.png";
 import coffee from "../assets/icons/coffee-shop.png";
 import graph from "../assets/images/graph.png";
+import data from "../src/data"
 
 
 export default function Index() {  
   const [mgCount, setMgCount] = useState(0);
   const resetMgCount = () => setMgCount(0);
 
+
+
   return (    
     <View className="flex justify-start min-h-full bg-white p-10">
       <StatusBar style="auto" />      
       
-      <View className="flex flex-row justify-between h-14 bg-red-50">
-        <Link className="bg-blue-400" href="/drinks">
-          <Image 
-            source={user}
-            className="w-[40px] h-[40px]"
-            resizeMode="contain"
-          />
+      <View className="flex flex-row justify-between h-[40px] mb-10">
+        <Link className="" href="/profile" asChild>
+          <Pressable>
+            <Image 
+              source={user}
+              className="w-[40px] h-[40px]"
+              resizeMode="contain"
+            />
+          </Pressable>
         </Link>
-        <Link className="bg-blue-400" href="/profile">
-          <Image 
-            source={coffee}
-            className="w-[40px] h-[40px]"
-            resizeMode="contain"
-          />
+        <Link className="" href="/drinks" asChild>
+          <Pressable>
+            <Image 
+              source={coffee}
+              className="w-[40px] h-[40px]"
+              resizeMode="contain"
+            />
+          </Pressable>
         </Link>
       </View>
-      
+
+
 
       <View className="flex justify-between flex-row">
         <Text className="font-pregular">Current caffeine amount: </Text>
@@ -46,51 +54,47 @@ export default function Index() {
           className="w-full h-52"
           resizeMode="shrink"
         />  
-      </View>      
-      
+      </View>  
+
 
 
       <View className="flex flex-wrap flex-row justify-between">
         <CustomButton 
-          title="Filter Coffee"
-          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 95)}
+          title={data.caffeineTypes[0].name}
+          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + data.caffeineTypes[0].mgPerCup)}
           containerStyles="h-20 p-2 w-20 mt-5"
         />
 
         <CustomButton 
-          title="Espresso"
-          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 27)}
+          title={data.caffeineTypes[1].name}
+          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + data.caffeineTypes[1].mgPerCup)}
           containerStyles="h-20 p-2 w-20 mt-5"
         />  
 
         <CustomButton 
-          title="Cappuccino"
-          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 27)}
+          title={data.caffeineTypes[2].name}
+          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + data.caffeineTypes[2].mgPerCup)}
           containerStyles="h-20 p-2 w-20 mt-5"
         />  
 
         <CustomButton 
-          title="Instant Coffee"
-          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 82)}
+          title={data.caffeineTypes[3].name}
+          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + data.caffeineTypes[3].mgPerCup)}
           containerStyles="h-20 p-2 w-20 mt-5"
         />  
 
         <CustomButton 
-          title="Decaf"
-          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + 15)}
+          title={data.caffeineTypes[4].name}
+          handlePress={onPress = () => setMgCount(prevMgCount => prevMgCount + data.caffeineTypes[4].mgPerCup)}
           containerStyles="h-20 p-2 w-20 mt-5"
         />
 
         <CustomButton
-        title="Reset"
-        handlePress={resetMgCount}
-        containerStyles="bg-blue-400 h-20 p-2 w-20 mt-5"
-        /> 
-        </View>
-
-        
-        
-
+          title="Reset"
+          handlePress={resetMgCount}
+          containerStyles="bg-blue-400 h-20 p-2 w-20 mt-5"
+        />
+      </View>      
     </View>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export default {
+  caffeineTypes: [
+    {
+      "name": "Filter Coffee",
+      "mgPerCup": 95
+    },
+    {
+      "name": "Espresso",
+      "mgPerCup": 27
+    },
+    {
+      "name": "Cappuccino",
+      "mgPerCup": 27
+    },
+    {
+      "name": "Instant Coffee",
+      "mgPerCup": 69
+    },
+    {
+      "name": "Decaf",
+      "mgPerCup": 5
+    }    
+  ]
+};
