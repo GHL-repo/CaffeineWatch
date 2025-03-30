f74d819a129288016fcacfff09b64825e960fd15
Guus <ghligtvoet@gmail.com>
2025-03-30 22:33:58 +0200

    new setting menu

--- app/settings.tsx
+++ app/settings.tsx
@@ -24,6 +24,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Pressable } from "react-native";
 import { Link } from "expo-router";
 import coffee from "../assets/icons/coffee-shop.png";
+import { ChevronRightIcon } from "react-native-heroicons/outline";
 
 export default function Profile() {
   const [cafTypes, setCafTypes] = useState(data.caffeineTypes);
@@ -49,21 +50,47 @@ export default function Profile() {
       <Header />
 
       <View className="h-full bg-white pt-2 pl-10 pr-10">
-        <Text className="text-xl font-bold mb-3">Settings</Text>
-        <Link className="mb-3" href="/drinks" asChild>
-          <Pressable>
-            <Image
-              source={coffee}
-              className="w-[40px] h-[40px]"
-              resizeMode="contain"
-            />
-          </Pressable>
-        </Link>
-        <Text className="text-sm font-normal mb-1">Timezone</Text>
-        <Text className="text-sm font-normal mb-1">Bedtime</Text>
-        <Text className="text-sm font-normal mb-1">Caffeine Tolerance</Text>
-        <Text className="text-sm font-normal mb-1">Warning</Text>
-        <Text className="text-sm font-normal mb-1">Day starts at</Text>
+        <Text className="text-xl font-bold mb-5">Settings</Text>
+
+        <View className="rounded-lg bg-gray-50 border border-gray-300 px-3 py-[19px] mb-2">
+          <Link className="" href="/drinks" asChild>
+            <Pressable className="flex-row justify-between items-center">
+              <Text className="text-lg font-normal">Customize Drinks Menu</Text>
+              <ChevronRightIcon color="black" fill="white" size={22} />
+            </Pressable>
+          </Link>
+        </View>
+
+        <View className="rounded-lg bg-gray-50 border border-gray-300 p-3 mb-2">
+          <Link className="" href="/settings" asChild>
+            <Pressable className="flex-row justify-between items-center">
+              <View>
+                <Text className="text-sm font-normal">Caffeine Tolerance</Text>
+                <Text className="text-sm font-normal text-blue-400">100mg</Text>
+              </View>
+              <ChevronRightIcon color="black" fill="white" size={22} />
+            </Pressable>
+          </Link>
+        </View>
+
+        <View className="rounded-lg bg-gray-50 border border-gray-300 p-3 mb-2">
+          <Link className="" href="/settings" asChild>
+            <Pressable className="flex-row justify-between items-center">
+              <View>
+                <Text className="text-sm font-normal">Theme</Text>
+                <Text className="text-sm font-normal text-blue-400">
+                  Light (default)
+                </Text>
+              </View>
+              <ChevronRightIcon color="black" fill="white" size={22} />
+            </Pressable>
+          </Link>
+        </View>
+        {/* <Text className="text-sm font-normal mb-1">Timezone</Text> */}
+        {/* <Text className="text-sm font-normal mb-1">Bedtime</Text> */}
+
+        {/* <Text className="text-sm font-normal mb-1">Warning</Text> */}
+        {/* <Text className="text-sm font-normal mb-1">Day starts at</Text> */}
       </View>
     </View>
   );
